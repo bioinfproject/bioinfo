8c029b026cde5711bc6e7bc54826df55dc181252
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2018-11-11 06:19:06 -0600

    Update GO.py

--- Folder/GO.py
+++ Folder/GO.py
@@ -3086,6 +3086,17 @@ if os.path.exists('HD.py'): os.remove('HD.py')
 if os.path.exists("./*.RData"): os.remove("./*.RData")
 if os.path.exists("*.RData"): os.remove("*.RData")
 if os.path.exists("./Rplots.pdf"): os.remove("./Rplots.pdf")
+import os, fnmatch
+def find(pattern,path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
+file_uniprot=find('*.Rout','./')
+for i in file_uniprot:
+    if os.path.exists(i): os.remove(i)
 
 # print total time of analysis
 lapso_total = datetime.now() - inicio_total
