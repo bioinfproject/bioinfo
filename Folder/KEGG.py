9c139fe0967bbd72567ff4c969c182cf8bb6a22f
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2018-11-13 13:31:42 -0600

    Update KEGG.py

--- Folder/KEGG.py
+++ Folder/KEGG.py
@@ -823,7 +823,7 @@ if os.path.exists('GO.py'): os.remove('GO.py')
 if os.path.exists('KEGG.py'): os.remove('KEGG.py')
 if os.path.exists('HD.py'): os.remove('HD.py')
 if os.path.exists("./*.RData"): os.remove("./*.RData")
-if os.path.exists("*.RData"): os.remove("*.RData")
+if os.path.exists(".RData"): os.remove(".RData")
 if os.path.exists("./Rplots.pdf"): os.remove("./Rplots.pdf")
 import os, fnmatch
 def find(pattern,path):
