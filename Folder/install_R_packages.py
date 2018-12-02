8c3eb8ebf379042c3e9de997a418bd6c60ac3734
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2018-12-02 03:47:40 -0600

    install_R_packages.py

---
+++ Folder/install_R_packages.py
@@ -0,0 +1,20 @@
+import urllib.request
+import shutil, os
+from urllib.request import urlopen
+import requests
+from time import sleep
+from subprocess import Popen, PIPE, STDOUT
+from subprocess import call
+import shlex, subprocess
+import subprocess
+import sys
+import warnings
+import os, fnmatch
+
+urllib.request.urlretrieve('https://raw.githubusercontent.com/bioinfproject/bioinfo/master/Folder/R_packages_for_NeVOmics.R', './R_packages_for_NeVOmics.R')
+
+subprocess.call(["setx","Path","Documents\R-3.5.1\bin;%Path%"])
+
+subprocess.call(["Rscript","--vanilla","R_packages_for_NeVOmics.R"])
+
+if os.path.exists('R_packages_for_NeVOmics.R'): os.remove('R_packages_for_NeVOmics.R')
