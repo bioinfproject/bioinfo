7a4045b17a96835af4c908057a7ebaac59ed46eb
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-03-21 22:32:13 -0500

    Update Install_R_packages_Ubuntu.py

--- Folder/Install_R_packages_Ubuntu.py
+++ Folder/Install_R_packages_Ubuntu.py
@@ -13,7 +13,7 @@ import sys
 import warnings
 import os, fnmatch
 
-urllib.request.urlretrieve('https://raw.githubusercontent.com/bioinfproject/bioinfo/master/Folder/R_packages_for_NeVOmics_Ubuntu.R', './R_packages_for_NeVOmics.R')
+urllib.request.urlretrieve('https://raw.githubusercontent.com/bioinfproject/bioinfo/master/Folder/proof_dir.R', './R_packages_for_NeVOmics.R')
 
 print('\n\nRunning: Rscript\n')
         
