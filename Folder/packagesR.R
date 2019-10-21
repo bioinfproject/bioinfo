2b859284b9a0e6c3d21b5b14f490793e11081c6f
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-10-20 23:05:44 -0500

    Update packagesR.R

--- Folder/packagesR.R
+++ Folder/packagesR.R
@@ -4,8 +4,8 @@
 #install.packages("GetoptLong", dependencies = TRUE,repos='http://cran.us.r-project.org')
 install.packages("tidyr", dependencies = TRUE,repos='http://cran.us.r-project.org')
 install.packages("circlize", dependencies = TRUE,repos='http://cran.us.r-project.org')
-#install.packages("RColorBrewer", dependencies = TRUE,repos='http://cran.us.r-project.org')
-#install.packages("gridBase",dependencies = TRUE,repos='http://cran.us.r-project.org')
+install.packages("RColorBrewer", dependencies = TRUE,repos='http://cran.us.r-project.org')
+install.packages("gridBase",dependencies = TRUE,repos='http://cran.us.r-project.org')
 source("http://bioconductor.org/biocLite.R")
 biocLite("ComplexHeatmap")
 #
