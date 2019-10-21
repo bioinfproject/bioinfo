1d13771638ffec574d0abf95c839aab5a8cd2f3f
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-10-20 23:04:14 -0500

    Update packagesR.R

--- Folder/packagesR.R
+++ Folder/packagesR.R
@@ -1,11 +1,11 @@
 
 .libPaths('Rlibrary_NeVOmics')
 #
-install.packages("GetoptLong", dependencies = TRUE,repos='http://cran.us.r-project.org')
-install.packages("tidyverse", dependencies = TRUE,repos='http://cran.us.r-project.org')
+#install.packages("GetoptLong", dependencies = TRUE,repos='http://cran.us.r-project.org')
+install.packages("tidyr", dependencies = TRUE,repos='http://cran.us.r-project.org')
 install.packages("circlize", dependencies = TRUE,repos='http://cran.us.r-project.org')
 #install.packages("RColorBrewer", dependencies = TRUE,repos='http://cran.us.r-project.org')
-install.packages("gridBase",dependencies = TRUE,repos='http://cran.us.r-project.org')
+#install.packages("gridBase",dependencies = TRUE,repos='http://cran.us.r-project.org')
 source("http://bioconductor.org/biocLite.R")
 biocLite("ComplexHeatmap")
 #
