4336c575b605fbcf0cb5083a873443a3fbd9d5df
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-11-04 19:45:22 -0500

    Update install_python_modules.py

--- Folder/install_python_modules.py
+++ Folder/install_python_modules.py
@@ -5,7 +5,7 @@ import subprocess
 
 print('\n\n Installation of modules used by NeVOmics\n\n')
 
-print('MODULE:  pip, tkinter, requests, pandas, scipy, openpyxl, colormap and networkx')
+print('MODULE:  pip, tkinter, requests, pandas, scipy, openpyxl, colormap, easydev and networkx')
 
 a=subprocess.Popen(['python','-m','pip','install','--upgrade','pip'])
 a.wait()
@@ -23,6 +23,9 @@ a=subprocess.Popen(['python','-m','pip','install','openpyxl'])
 a.wait()
 
 a=subprocess.Popen(['python','-m','pip','install','colormap'])
+a.wait()
+
+a=subprocess.Popen(['python','-m','pip','install','easydev'])
 a.wait()
 
 a=subprocess.Popen(['python','-m','pip','install','--user','networkx'])
