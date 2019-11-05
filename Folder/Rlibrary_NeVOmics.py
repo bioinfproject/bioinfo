cb11e3003ef093ed5589ab48af8f153b65202c51
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-11-05 02:29:30 -0500

    Update Rlibrary_NeVOmics.py

--- Folder/Rlibrary_NeVOmics.py
+++ Folder/Rlibrary_NeVOmics.py
@@ -3,5 +3,5 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 print('\n\n!!! This may take several minutes !!!\n\n')
 
 gdd.download_file_from_google_drive(file_id='1u9PkZ5UWcKeOaJGjhS-VYm0WIz0vd6cP',
-                                    dest_path='./panther.zip',
+                                    dest_path='./Rlibrary_NeVOmics.zip',
                                     unzip=True)
