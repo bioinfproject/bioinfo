25f4aad66cae67f6b2b3afab39e62b1a9fcbc609
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-11-05 01:42:02 -0500

    Add files via upload

---
+++ Folder/Rlibrary_NeVOmics.py
@@ -0,0 +1,7 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+print('\n\n!!! This may take several minutes !!!\n\n')
+
+gdd.download_file_from_google_drive(file_id='1u9PkZ5UWcKeOaJGjhS-VYm0WIz0vd6cP',
+                                    dest_path='./panther.zip',
+                                    unzip=True)
