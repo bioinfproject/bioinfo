f9b9840702b17c2f0abf0fc04e61cbffe0281d76
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2019-11-06 07:15:22 -0500

    Update Rpackages.R

--- Folder/Rpackages.R
+++ Folder/Rpackages.R
@@ -1,14 +1,14 @@
 #
-install.packages("xml2", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("curl", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("httr", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("xml2", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("curl", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("httr", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
 #
-install.packages("GetoptLong", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("tidyverse", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("circlize", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("RColorBrewer", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("gridBase",lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("ComplexHeatmap", lib = "~/R-NeVOmics", repos = "https://bioconductor.org/packages/release/bioc/")
+install.packages("GetoptLong", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("tidyverse", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("circlize", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("RColorBrewer", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("gridBase",lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("ComplexHeatmap", lib = "~/Rlibrary_NeVOmics", repos = "https://bioconductor.org/packages/release/bioc/")
 #
 cat("\n\n!!! All packages have been installed successfully!!!\n\n")
 
