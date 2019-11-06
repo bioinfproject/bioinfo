a53a664dcf0fbb73e2d222289249a2fd358b5758
bioinfproject <pgen10@hotmail.com>
2019-11-06 07:03:06 -0500

    update

---
+++ Folder/Rpackages.R
@@ -0,0 +1,15 @@
+#
+install.packages("xml2", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("curl", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("httr", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+#
+install.packages("GetoptLong", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("tidyverse", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("circlize", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("RColorBrewer", lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("gridBase",lib = "~/R-NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("ComplexHeatmap", lib = "~/R-NeVOmics", repos = "https://bioconductor.org/packages/release/bioc/")
+#
+cat("\n\n!!! All packages have been installed successfully!!!\n\n")
+
+
