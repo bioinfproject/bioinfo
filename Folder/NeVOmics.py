fa99de13162f0d4091d2f9007b3713e49d1bed5c
eduardo1011 <35233945+eduardo1011@users.noreply.github.com>
2019-11-16 22:02:51 -0600

    update

--- Folder/NeVOmics.py
+++ Folder/NeVOmics.py
@@ -695,7 +695,7 @@ diez = Label(group_aspect22, text= '             Local Blast: ', font=("Courier 
 diez.grid(column=1, row=15, sticky= E)
 
 keggblmethod = IntVar()#--------------------------------------------------------------
-tipos = ['Blastx', 'Blastp']
+tipos = ['Blastp', 'Blastx']
 #mets = {0:2,1:3}
 mets = {0:1,1:2}
 #span = {0:1,1:1}
