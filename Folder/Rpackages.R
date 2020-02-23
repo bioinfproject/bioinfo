c0300a34f890f6e6380b2ab633992185256c783c
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2020-02-23 13:47:46 -0600

    Update Rpackages.R

--- Folder/Rpackages.R
+++ Folder/Rpackages.R
@@ -1,17 +1,17 @@
 #
-install.packages("xml2", lib = "~/Rlibrary_NeVOmics")
-install.packages("curl", lib = "~/Rlibrary_NeVOmics")
-install.packages("httr", lib = "~/Rlibrary_NeVOmics")
+install.packages("xml2", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("curl", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("httr", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
 #
-install.packages("GetoptLong", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("tidyverse", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("circlize", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("RColorBrewer", lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("gridBase",lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("png",lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
-install.packages("clue",lib = "~/Rlibrary_NeVOmics" , repos='http://cran.us.r-project.org')
+install.packages("GetoptLong", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("tidyverse", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("circlize", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("RColorBrewer", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("gridBase",lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("png",lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
+install.packages("clue",lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos='http://cloud.r-project.org')
 
-install.packages("ComplexHeatmap", lib = "~/Rlibrary_NeVOmics", repos = "https://bioconductor.org/packages/release/bioc/")
+install.packages("ComplexHeatmap", lib = "~/Rlibrary_NeVOmics", dependencies = TRUE, repos = "https://bioconductor.org/packages/release/bioc/")
 #
 cat("\n\n!!! All packages have been installed successfully!!!\n\n")
 
