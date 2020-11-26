74582b5d1eabb36f9fe8c4148897d9291ff45192
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2020-11-26 09:01:02 -0600

    Update short_KEGG_Ubuntu_Blast.py

--- Folder/short_KEGG_Ubuntu_Blast.py
+++ Folder/short_KEGG_Ubuntu_Blast.py
@@ -1618,7 +1618,10 @@ if createnetworks == '1':
             plt.text(i, 19.8, '|', size= 4, ha='center')
         
     
-        plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black') #fontweight='bold'
+        if val_max_to_scala > 30:
+            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+        else:
+            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
         
         
         plt.yticks(color='none') # oculta las etiquetas del eje y
@@ -1768,7 +1771,10 @@ if createnetworks == '1':
             plt.text(i, 19.8, '|', size= 4, ha='center')
         
     
-        plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black') #fontweight='bold'
+        if val_max_to_scala > 30:
+            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+        else:
+            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
         
         
         plt.yticks(color='none') # oculta las etiquetas del eje y
@@ -1873,7 +1879,10 @@ if createnetworks == '1':
             plt.text(i, 19.8, '|', size= 4, ha='center')
         
     
-        plt.xticks([-15] + [val_max_to_scala], size=6, color='black') #fontweight='bold'
+        if val_max_to_scala > 30:
+            plt.xticks([-15] + [val_max_to_scala*2], size=6, color='black')
+        else:
+            plt.xticks([-15] + [val_max_to_scala], size=6, color='black')
         
         plt.yticks(list(range(1,25)), color='none') # aumento el margen superior para mostrar el titulo
         #plt.yticks(color='none') # oculta las etiquetas del eje y
@@ -2141,7 +2150,10 @@ if createnetworks == '1':
             plt.text(i, 19.8, '|', size= 4, ha='center')
         
     
-        plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black') #fontweight='bold'
+        if val_max_to_scala > 30:
+            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+        else:
+            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
         
         
         plt.yticks(color='none') # oculta las etiquetas del eje y
@@ -2269,7 +2281,10 @@ if createnetworks == '1':
             plt.text(i, 19.8, '|', size= 4, ha='center')
         
     
-        plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black') #fontweight='bold'
+        if val_max_to_scala > 30:
+            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+        else:
+            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
         
         
         plt.yticks(color='none') # oculta las etiquetas del eje y
