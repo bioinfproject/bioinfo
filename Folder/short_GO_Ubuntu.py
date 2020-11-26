417808266fbe8ddfeb774f30840880424f764e2c
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2020-11-26 09:13:15 -0600

    Update short_GO_Ubuntu.py

--- Folder/short_GO_Ubuntu.py
+++ Folder/short_GO_Ubuntu.py
@@ -1529,9 +1529,9 @@ def create_plots(XXXXXXXXXX = DataFrame([]),
             plt.text(i, 20.2, str(i), size= 6, ha='center')
             plt.text(i, 19.8, '|', size= 4, ha='center')
         if val_max_to_scala > 30:
-            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*1.5], size=6, color='black')
         else:
-            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*3], size=6, color='black')
         plt.yticks(color='none') # oculta las etiquetas del eje y
         ax3.axis('off')
         #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -1640,9 +1640,9 @@ def create_plots(XXXXXXXXXX = DataFrame([]),
             plt.text(i, 20.2, str(i), size= 6, ha='center')
             plt.text(i, 19.8, '|', size= 4, ha='center')
         if val_max_to_scala > 30:
-            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*1.5], size=6, color='black')
         else:
-            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')    
+            plt.xticks([-2.5] + [val_max_to_scala*3], size=6, color='black')    
         plt.yticks(color='none') # oculta las etiquetas del eje y
         ax3.axis('off')
         #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -1720,9 +1720,9 @@ def create_plots(XXXXXXXXXX = DataFrame([]),
             plt.text(i, 20.2, str(i), size= 6, ha='center')
             plt.text(i, 19.8, '|', size= 4, ha='center')
         if val_max_to_scala > 30:
-            plt.xticks([-15] + [val_max_to_scala*2], size=6, color='black')
+            plt.xticks([-15] + [val_max_to_scala*1.5], size=6, color='black')
         else:
-            plt.xticks([-15] + [val_max_to_scala], size=6, color='black')
+            plt.xticks([-15] + [val_max_to_scala*3], size=6, color='black')
         plt.yticks(list(range(1,25)), color='none') # aumento el margen superior para mostrar el titulo
         #plt.yticks(color='none') # oculta las etiquetas del eje y
         ax.axis('off')
@@ -1913,9 +1913,9 @@ def create_plots(XXXXXXXXXX = DataFrame([]),
             plt.text(i, 20.2, str(i), size= 6, ha='center')
             plt.text(i, 19.8, '|', size= 4, ha='center')
         if val_max_to_scala > 30:
-            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*1.5], size=6, color='black')
         else:
-            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*3], size=6, color='black')
         plt.yticks(color='none') # oculta las etiquetas del eje y
         ax3.axis('off')
         #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -2009,9 +2009,9 @@ def create_plots(XXXXXXXXXX = DataFrame([]),
             plt.text(i, 20.2, str(i), size= 6, ha='center')
             plt.text(i, 19.8, '|', size= 4, ha='center')
         if val_max_to_scala > 30:
-            plt.xticks([-2.5] + [val_max_to_scala*2], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*1.5], size=6, color='black')
         else:
-            plt.xticks([-2.5] + [val_max_to_scala], size=6, color='black')
+            plt.xticks([-2.5] + [val_max_to_scala*3], size=6, color='black')
         plt.yticks(color='none') # oculta las etiquetas del eje y
         ax3.axis('off')
         #■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
