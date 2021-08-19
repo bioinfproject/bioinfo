659280b7166b740a42f36cd0de656353260f1b1b
bioinfproject <42109887+bioinfproject@users.noreply.github.com>
2021-08-19 00:02:56 -0500

    Update GUINevomics_Windows.py

--- Folder/GUINevomics_Windows.py
+++ Folder/GUINevomics_Windows.py
@@ -408,7 +408,7 @@ root = Tk()
 #root.tk.call('tk', 'scaling', 1.5)
 root.title("NeVOmics")
 root.iconbitmap(r'NeVOmics_img/icon_nevomics.ico')
-root.geometry("760x630")
+root.geometry("900x720")
 #root.resizable(0, 0) # fija las dimensiones de la ventana
 root.configure(background='white')
 
